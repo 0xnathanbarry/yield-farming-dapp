1deee033d7f913316b070b9b09f534b17ae6e41e
Nathan Barry <nathanbarry474@gmail.com>
2021-10-20 22:40:17 -0500

    finished it

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import Web3 from "web3";
+import DaiToken from "../abis/DaiToken.json";
+import DappToken from "../abis/DappToken.json";
+import TokenFarm from "../abis/TokenFarm.json";
 import Navbar from "./Navbar";
+import Main from "./Main";
 import "./App.css";
 
 class App extends Component {
@@ -14,6 +18,58 @@ class App extends Component {
 
     const accounts = await web3.eth.getAccounts();
     this.setState({ account: accounts[0] });
+
+    const networkId = await web3.eth.net.getId();
+
+    // Load DaiToken
+    const daiTokenData = DaiToken.networks[networkId];
+    if (daiTokenData) {
+      const daiToken = new web3.eth.Contract(
+        DaiToken.abi,
+        daiTokenData.address
+      );
+      this.setState({ daiToken });
+      let daiTokenBalance = await daiToken.methods
+        .balanceOf(this.state.account)
+        .call();
+      this.setState({ daiTokenBalance: daiTokenBalance.toString() });
+    } else {
+      window.alert("DaiToken contract not deployed to detected network.");
+    }
+
+    // Load DappToken
+    const dappTokenData = DappToken.networks[networkId];
+    if (dappTokenData) {
+      const dappToken = new web3.eth.Contract(
+        DappToken.abi,
+        dappTokenData.address
+      );
+      this.setState({ dappToken });
+      let dappTokenBalance = await dappToken.methods
+        .balanceOf(this.state.account)
+        .call();
+      this.setState({ dappTokenBalance: dappTokenBalance.toString() });
+    } else {
+      window.alert("DappToken contract not deployed to detected network.");
+    }
+
+    // Load TokenFarm
+    const tokenFarmData = TokenFarm.networks[networkId];
+    if (tokenFarmData) {
+      const tokenFarm = new web3.eth.Contract(
+        TokenFarm.abi,
+        tokenFarmData.address
+      );
+      this.setState({ tokenFarm });
+      let stakingBalance = await tokenFarm.methods
+        .stakingBalance(this.state.account)
+        .call();
+      this.setState({ stakingBalance: stakingBalance.toString() });
+    } else {
+      window.alert("TokenFarm contract not deployed to detected network.");
+    }
+
+    this.setState({ loading: false });
   }
 
   async loadWeb3() {
@@ -28,6 +84,31 @@ class App extends Component {
       );
     }
   }
+
+  stakeTokens = (amount) => {
+    this.setState({ loading: true });
+    this.state.daiToken.methods
+      .approve(this.state.tokenFarm._address, amount)
+      .send({ from: this.state.account })
+      .on("transactionHash", (hash) => {
+        this.state.tokenFarm.methods
+          .stakeTokens(amount)
+          .send({ from: this.state.account })
+          .on("transactionHash", (hash) => {
+            this.setState({ loading: false });
+          });
+      });
+  };
+
+  unstakeTokens = (amount) => {
+    this.setState({ loading: true });
+    this.state.tokenFarm.methods
+      .unstakeTokens()
+      .send({ from: this.state.account })
+      .on("transactionHash", (hash) => {
+        this.setState({ loading: false });
+      });
+  };
 
   constructor(props) {
     super(props);
@@ -44,6 +125,25 @@ class App extends Component {
   }
 
   render() {
+    let content;
+    if (this.state.loading) {
+      content = (
+        <p id="loader" className="text-center">
+          Loading...
+        </p>
+      );
+    } else {
+      content = (
+        <Main
+          daiTokenBalance={this.state.daiTokenBalance}
+          dappTokenBalance={this.state.dappTokenBalance}
+          stakingBalance={this.state.stakingBalance}
+          stakeTokens={this.stakeTokens}
+          unstakeTokens={this.unstakeTokens}
+        />
+      );
+    }
+
     return (
       <div>
         <Navbar account={this.state.account} />
@@ -61,7 +161,7 @@ class App extends Component {
                   rel="noopener noreferrer"
                 ></a>
 
-                <h1>Hello, World!</h1>
+                {content}
               </div>
             </main>
           </div>
